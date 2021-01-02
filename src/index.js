96d84d7e7ae6f2e7d3b6dbfeab522a017c2d2acb
jhygreatbug <jhygreatbug@gmail.com>
2021-01-02 16:40:43 +0800

    feat: geo (unfinished)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import Vue from './vue.esm.browser.js'
+
 function getTime(hourRef) {
   const isAm = 1 <= hourRef && hourRef <= 12
   let hours = hourRef % 12
@@ -12,6 +14,58 @@ function getTime(hourRef) {
 
 function getCurrentHours() {
   return new Date().getHours()
+}
+
+function requestWeather() {
+  return fetch('http://example.com/movies.json')
+    .then(response => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      const res = response.json()
+      // return res.weather
+      return ['sunny', 'rainy', 'snowy'][Math.floor(Math.random() * 3)]
+    })
+    .catch(e => {
+      console.log('weather error', e)
+      return 'sunny'
+    })
+}
+
+function requestGeo() {
+  return new Promise((resolve, reject) => {
+    // todo: google maps platform
+    if (true) {
+      fetch('https://api.map.baidu.com/location/ip?ak=kf5mCAHrpjyC9ZB1T9IatxcmpYBqF7nD&coor=bd09ll')
+        .then(response => {
+          if (!response.ok) {
+            throw Error(response.statusText);
+          }
+          const res = response.json()
+          return res
+        })
+        .then(res => {
+          if (res.status !== 0 || !res.content || !res.content.point) {
+            // todo: 具体的错误信息
+            throw new Error(res.status)
+          }
+          resolve(res.content.point)
+        })
+        .catch(reject)
+    } else {
+      navigator.geolocation.getCurrentPosition(position => {
+        resolve({
+          x: position.coords.latitude,
+          y: position.coords.longitude
+        })
+      }, (e) => {
+        console.log('geolocation error', e)
+        reject(e)
+      }, {
+        timeout: 10000
+      });
+    }
+  })
 }
 
 const ComponentMyAudio = {
@@ -88,7 +142,10 @@ var vm = new Vue({
   data() {
     const selectHoursOptions = Array.from(
       { length: 24 },
-      (v, i) => getTime(i)
+      (v, i) => ({
+        value: i,
+        ...getTime(i),
+      })
     )
     return {
       selectHoursOptions,
@@ -104,9 +161,27 @@ var vm = new Vue({
   computed: {
     playMode() {
       return this.inputPlayMode ? 'simulate' : 'custom'
-    }
+    },
+  },
+  created() {
+    this.updateWeather()
   },
   methods: {
+    updateWeather() {
+      requestGeo()
+        .then(coords => {
+          console.log('coords', coords.x, coords.y)
+          requestWeather(coords)
+            .then(weather => {
+              this.currentWeather = weather
+              this.audioStatus.weather = weather
+            })
+        })
+        .catch((e) => {
+          console.log('failed', e)
+          alert('获取位置信息失败')
+        })
+    },
     reset() {
       this.audioStatus.hours = getCurrentHours()
       this.audioStatus.weather = this.currentWeather
@@ -114,10 +189,19 @@ var vm = new Vue({
     handleAudioReady($audio) {
       this.$audio = $audio
       this.$audio.play()
-      setTimeout(() => {
+      setInterval(() => {
         const hours = getCurrentHours()
-        if (this.playMode === 'simulate' && hours !== this.audioStatus.hours) {
-          this.audioStatus.hours = hours
+        if (this.playMode === 'simulate') {
+          const seconds = new Date().getSeconds()
+          const minutes = new Date().getMinutes()
+          if (seconds === 0 && minutes === 0) {
+            // 整点更新天气
+            this.updateWeather()
+          }
+          if (hours !== this.audioStatus.hours) {
+            // 整点更新BGM
+            this.audioStatus.hours = hours
+          }
         }
       }, 950)
     },
